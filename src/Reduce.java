9b6b6250db53ddb2474698fc6d9bdb01f725e671
nadjamarjanovic <129619501+nadjamarjanovic@users.noreply.github.com>
2023-09-13 21:02:44 -0400

    more changes

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,7 +1,10 @@
 /** Starting with a number n, if n is even divide it by 2. If n is odd, subtract 1. Repeat
  * write a main method that prints how many steps it takes to reach 0 if you start at 100 */
 public class Reduce {
-    public static int reduce(int n) {
+    public static void main(String[] args) {
+        System.out.print(main(100));
+    }
+    public static int main(int n) {
         int count = 0;
         int i = n;
         while (i > 0) {
@@ -18,8 +21,5 @@ public class Reduce {
             count ++;
         }
         return count;
-    }
-    public static void main(String[] args) {
-        System.out.print(reduce(100));
     }
 }
